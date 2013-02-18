ddca03e2dcecba88a6c19535cc03b3055848d7fc
Erik Streufert <streer02@luther.edu>
2013-02-18 13:44:39 -0600

    Red Style css

--- web2py/applications/SeniorProject/controllers/default.py
+++ web2py/applications/SeniorProject/controllers/default.py
@@ -31,7 +31,7 @@ else:
 
 projects = db(db.Project).select()
 
-header = DIV(A(IMG(_src=URL('static','images/stock.jpeg')), _href=URL('default','index')), _id="header")
+header = DIV(A(IMG(_src=URL('static','images/redbannertext.jpg')), _href=URL('default','index')), _id="header")
 footer = DIV("This website brought to you by the Supreme Leader, Minion #3 (Alysse), Minion #2 (Scott), and the No-Longer-Sick One (Erik)", _id="footer")
 
 @auth.requires_login()
